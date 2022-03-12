9753509d54a2888c11c989c3da5b3af36cde21e3
Klaus Lehner <172195+klu2@users.noreply.github.com>
2022-03-12 20:34:00 +0100

    add readFromFile method for NpmPackageLock

--- src/main/kotlin/io/cloudflight/jsonwrapper/npm/NpmPackageLock.kt
+++ src/main/kotlin/io/cloudflight/jsonwrapper/npm/NpmPackageLock.kt
@@ -1,13 +1,25 @@
 package io.cloudflight.jsonwrapper.npm
 
+import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
+import io.cloudflight.jsonwrapper.Parser
+import java.io.File
 
 /**
- * model of a package.json file
+ * A model of a `package-lock.json` file as described here: https://docs.npmjs.com/cli/v8/configuring-npm/package-lock-json
+ *
+ * @author Klaus Lehner
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
-class NpmPackageLock {
-    lateinit var name: String
-    lateinit var version: String
-    var dependencies: Map<String, NpmDependency> = emptyMap()
+class NpmPackageLock @JsonCreator constructor(
+    @JsonProperty("name") val name: String,
+    @JsonProperty("version") val version: String,
+    @JsonProperty("dependencies") val dependencies: Map<String, NpmDependency> = emptyMap()
+) {
+    companion object {
+        fun readFromFile(file: File): NpmPackageLock {
+            return Parser.parseFile(file, NpmPackageLock::class.java)
+        }
+    }
 }
