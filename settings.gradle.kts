d15d9724f87b16d115ff461acb81f3d3e48b17cc
Klaus Lehner <172195+klu2@users.noreply.github.com>
2022-11-07 09:19:09 +0100

    fix github actions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,8 +2,4 @@ plugins {
     id("io.cloudflight.autoconfigure-settings") version "0.8.3"
 }
 
-configure<org.ajoberstar.reckon.gradle.ReckonExtension>() {
-    setDefaultInferredScope("patch")
-}
-
 rootProject.name = "json-wrapper"
